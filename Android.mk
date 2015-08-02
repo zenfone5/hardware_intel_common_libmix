20efb42e70e71d0031d173615a929ee66c414861
Matchbox Bazz <knone.null@gmail.com>
2015-08-02 08:24:23 +0800

    fix build

--- Android.mk
+++ Android.mk
@@ -4,4 +4,6 @@ ifeq ($(INTEL_VA),true)
  include $(AUDIO_PATH)/mixvbp/Android.mk
  include $(AUDIO_PATH)/videodecoder/Android.mk
  include $(AUDIO_PATH)/videoencoder/Android.mk
+ include $(AUDIO_PATH)/imageencoder/Android.mk
+ include $(AUDIO_PATH)/imagedecoder/Android.mk
 endif
